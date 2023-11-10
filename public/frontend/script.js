735ab3122552b360dbd48ac0a5942e73f5836d61
Nandha <nandha.kumar790@gmail.com>
2023-11-10 18:57:45 +0530

    search feature implemented

--- public/frontend/script.js
+++ public/frontend/script.js
@@ -1,6 +1,12 @@
-$(function (){
-    
-});
+var movArr = [];
+var tvArr = [];
+
+$(function(){
+    $('#download-list-form').on('submit', ()=>{
+        $('#mov-json-data').val(JSON.stringify(movArr));
+        $('#tv-json-data').val(JSON.stringify(tvArr));
+    })
+})
 
 const loadMovieData = e => {
     let mediaType = $('#media-select').val();
@@ -8,16 +14,6 @@ const loadMovieData = e => {
     if (query.length < 3)
         return false;
     let url= '';
-    switch (mediaType) {
-        case "1":
-            url = `https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=true&language=en-US&page=1`;
-            break;
-        case "2":
-            url = `https://api.themoviedb.org/3/search/tv?query=${query}&include_adult=true&language=en-US&page=1`;
-            break;
-        default:
-            console.log('default');
-    }
 
     const options = {
         method: 'GET',
@@ -26,9 +22,54 @@ const loadMovieData = e => {
           Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNzU2YTNmOTk4MWMzMjRmMDE3MDI5MTY4MmUwNzQ2ZSIsInN1YiI6IjYzMzBiMWRlYTVkODQ5MDA5MjU1OTY2ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.VyrNPvyjIohhS-gGcJxBJbfAar6xJNMljeJOkU35NkU'
         }
       };
-      
-      fetch(url, options)
-        .then(response => response.json())
+
+    switch (mediaType) {
+        case "1":
+            fetch(`https://api.themoviedb.org/3/search/movie?query=${query}&include_adult=true&language=en-US&page=1`, options)
+        .then(response => {
+            { 
+                let data = response.json();
+                $('#movie-select').empty().trigger("change");
+                data.then(data => {
+                 if(data.results){
+                    let results = data.results;
+                    movArr = [...movArr, ...results];
+                     for (const item of results){
+                         var newOption = new Option(item.title, item.id, true, true);
+                         // Append it to the select
+                         $('#movie-select').append(newOption).trigger('change');
+                     }
+                 }
+                 }, fail => console.log(fail)).catch(err => console.error(err));
+             }
+        })
         .then(response => console.log(response))
         .catch(err => console.error(err));
+            break;
+        case "2":
+            fetch(`https://api.themoviedb.org/3/search/tv?query=${query}&include_adult=true&language=en-US&page=1`, options)
+        .then(response => {
+            { 
+                let data = response.json();
+                $('#movie-select').empty().trigger("change");
+                data.then(data => {
+                 if(data.results){
+                    let results = data.results;
+                    tvArr = [...tvArr, ...results];
+                     for (const item of results){
+                         var newOption = new Option(item.name, item.id, true, true);
+                         // Append it to the select
+                         $('#movie-select').append(newOption).trigger('change');
+                     }
+                 }
+                 }, fail => console.log(fail)).catch(err => console.error(err));
+             }
+        })
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+            break;
+        default:
+            console.log('default');
+    }
+
 }
